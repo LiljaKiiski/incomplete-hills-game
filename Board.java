94691f385bed62f07fa328b754f75efa8cd15f2f
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-08-28 14:21:06 -0700

    Save

---
+++ Board.java
@@ -0,0 +1,18 @@
+public class Board {
+	public Hills hills;
+
+	public int x;
+	public int y;
+
+	public Board(Hills hills){
+		this.x = 0;
+		this.y = 0;
+
+		this.hills = hills;
+	}
+
+	public void move(){
+
+
+	}
+}
